de8c82f928b1f5bf2dbfe8c922eae6d20620c9fa
Cemalll <101356656+Cemalll@users.noreply.github.com>
2023-08-02 00:26:44 +0300

    Update ft_strstr.c

--- ex05/ft_strstr.c
+++ ex05/ft_strstr.c
@@ -13,19 +13,24 @@
 char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
-	int j;
+	int tofind;
 
 	i = 0;
-	if (to_find[0] == '\0')
+	tofind = 0;
+	if (to_find[i] == '\0')
+	{
 		return (str);
+	}
 	while (str[i] != '\0')
 	{
-		j = 0;
-		while (str[i + j] == to_find[j] && str[i + j] != '\0')
+		tofind = 0;
+		while (str[i + tofind] == to_find[tofind])
 		{
-			if (to_find[j + 1] == '\0')
-				return (&str[i]);
-			j++;
+			if (to_find[tofind + 1] == '\0')
+			{
+				return (str + i);
+			}
+			tofind++;
 		}
 		i++;
 	}
