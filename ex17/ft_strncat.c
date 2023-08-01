9e374906d400f4b78918b10e30166221b91c2b19
Cemalll <101356656+Cemalll@users.noreply.github.com>
2023-08-02 01:19:41 +0300

    Update ft_strncat.c

--- ex17/ft_strncat.c
+++ ex17/ft_strncat.c
@@ -12,18 +12,21 @@
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
+	int	i;
 	unsigned int	j;
-	unsigned int	i;
 
 	i = 0;
-	j = 0;
 	while (dest[i])
+	{
 		i++;
+	}
+	j = 0;
 	while (src[j] && j < nb)
 	{
-		dest[i + j] = src[j];
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
-	dest[i + j] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
