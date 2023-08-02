743004ac1d90a1badc86ec726a0ef658c71946c1
Cemalll <101356656+Cemalll@users.noreply.github.com>
2023-08-02 11:25:03 +0300

    Update ft_strstr.c

--- ex05/ft_strstr.c
+++ ex05/ft_strstr.c
@@ -9,30 +9,24 @@
 /*   Updated: 2023/08/01 15:36:07 by hayortan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stddef.h>
 
-char *ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
-    if (to_find[0] == '\0')
-        return str;
+	int	i;
+	int	j;
 
-    size_t i = 0;
-    size_t j;
-
-    while (str[i] != '\0')
-    {
-        j = 0;
-        while (str[i + j] == to_find[j] && str[i + j] != '\0' && to_find[j] != '\0')
-        {
-            j++;
-        }
-
-        if (to_find[j] == '\0')
-            return &str[i];
-
-        i++;
-    }
-
-    return NULL; // Instead of 0, return NULL for pointer types.
+	i = 0;
+	j = 0;
+	if (to_find[j] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		while (str[i + j] == to_find[j] && str[i + j] != '\0')
+			j++;
+		if (to_find[j] == '\0')
+			return (str + i);
+		j = 0;
+		i++;
+	}
+	return (0);
 }
-
