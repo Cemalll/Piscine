a3a57e1ecd278e776b61fd3d8320f6da629e3a2c
Cemalll <101356656+Cemalll@users.noreply.github.com>
2023-08-02 11:26:14 +0300

    Update ft_strcat.c

--- ex16/ft_strcat.c
+++ ex16/ft_strcat.c
@@ -13,18 +13,20 @@
 char	*ft_strcat(char *dest, char *src)
 {
 	int	i;
-	int	k;
+	int	j;
 
 	i = 0;
-	k = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[k] != '\0')
+	j = 0;
+	while (dest[j] != '\0')
 	{
-		dest[i] = src[k];
-		i++;
-		k++;
+		j++;
 	}
-	dest[i] = '\0';
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
 	return (dest);
 }
