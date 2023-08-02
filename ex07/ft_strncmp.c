37342f9ccee0c9a41e4cd7572f558858c7d1ebcf
Cemalll <101356656+Cemalll@users.noreply.github.com>
2023-08-02 11:25:45 +0300

    Update ft_strncmp.c

--- ex07/ft_strncmp.c
+++ ex07/ft_strncmp.c
@@ -15,13 +15,18 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (s1[i] > s2[i])
+		{
+			return (1);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (-1);
+		}
 		i++;
 	}
-	if (i == n)
-		return (0);
-	return (s1[i] - s2[i]);
+	return (0);
 }
+
