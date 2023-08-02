2c521a7b7019941be2853358a765d8055259f7e1
Cemalll <101356656+Cemalll@users.noreply.github.com>
2023-08-02 13:00:13 +0300

    Add files via upload

---
+++ ex20/ft_putnbr_base.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hayortan <hayortan@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/30 15:37:24 by hayortan          #+#    #+#             */
+/*   Updated: 2023/07/30 15:40:23 by hayortan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!base || base[0] == '\0' || base[1] == '\0')
+		return (0);
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == 127 || base[i] <= 32)
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	len;
+
+	len = 0;
+	while (base[len] != '\0')
+		len++;
+	if (check_base(base))
+	{
+		if (nbr < 0)
+		{
+			write(1, "-", 1);
+			nbr = -nbr;
+		}
+		if (nbr >= len)
+		{
+			ft_putnbr_base(nbr / len, base);
+			ft_putnbr_base(nbr % len, base);
+		}
+		if (nbr < len)
+			write(1, &base[nbr], 1);
+	}
+}
